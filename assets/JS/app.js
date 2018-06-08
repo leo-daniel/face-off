397d2cc6c9b95c1d33a60c76d44f7e415f83569e
Jordan <0jordanmiller@gmail.com>
2018-06-08 18:39:23 -0400

    jQuery added

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,15 +1,3 @@
-// Detect - generate face kye
-const dBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/detect?';
-const apiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
-const apiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
-let dImageURL = 'image_url=https://pbs.twimg.com/profile_images/650100474226458625/-nngF2Y1_400x400.jpg';
-const aBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/face/analyze?';
-
-let faceId;
-let aFaceId = '&face_tokens=' + faceId;
-let dQueryURL = dBaseURL + apiKey + apiSecret + dImageURL;
-const emotion = "&return_attributes=emotion";
-
 ////// ID's for HTML tags //////
 // USER INPUT URLS: #user-input1 #user-input-2
 // EMOTION DROPDOWN: #emotion-dropdown
@@ -18,8 +6,52 @@ const emotion = "&return_attributes=emotion";
 // SCORES: wins-1 wins-2 losses-1 lossed-2
 
 $(document).ready(function() {
+    // Detect - generate face key
+    // Replaced the old API key with Jordan's API key, to make sure we aren't sending too many requests
+const apiKey = 'api_key=4AW8dHolxjkb_gEXzZJp8TNc_XG3SBDF';
+const apiSecret = '&api_secret=clKHyanKY2E8wf7CgphWTZgAhTJdUbae';
+const dBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/detect?';
+let dImageURL = '&image_url=https://pbs.twimg.com/profile_images/650100474226458625/-nngF2Y1_400x400.jpg';
+let dQueryURL = dBaseURL + apiKey + apiSecret + dImageURL;
 
-initialAjax();
+
+const aBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/face/analyze?';
+let faceId;
+let aFaceId = '&face_tokens=' + faceId;
+const emotion = "&return_attributes=emotion";
+
+    // jquery
+
+$("#submit-1").on('click', function() {
+    let playerOneURL = $("#user-input-1").val().trim();
+    $("#image-1").attr('src', playerOneURL);
+    let imgURL1 = 'image_url=' + playerOneURL;
+});
+
+$("#submit-2").on('click', function() {
+    let playerTwoURL = $("#user-input-2").val().trim();
+    $("#image-2").attr('src', playerTwoURL);
+    let imgURL2 = 'image_url=' + playerTwoURL;
+});
+
+$("#play-button").on('click', function() {
+    let imgOneSrc = $("#image-1").attr('src')
+    let imgTwoSrc = $("#image-2").attr('src')
+    let imgOneSrcAPI = '&image_url' + imgOneSrc;
+    let ImgTwoSrcAPI = '&image_url' + imgTwoSrc;
+    let selectEmotion = $("#emotion-dropdown").find(":selected").text();    
+
+    // This stops the game from playing if the user hasn't uploaded any images and/or hasn't selected an emotion from the dropdown
+    if (imgOneSrc === 'http://via.placeholder.com/200' || imgTwoSrc === 'http://via.placeholder.com/200' || selectEmotion === 'Select an emotion...') {
+        console.log('Please input two images and select an emotion');
+    } else {
+        let dQueryURL = dBaseURL + apiKey + apiSecret + imgOneSrcAPI;
+        initialAjax();
+    }
+});
+
+// ajax
+// initialAjax();
 
 function initialAjax() {
     console.log('initial Ajax fired');
