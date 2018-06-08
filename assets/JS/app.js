cdd961f9973129fea0be88450e8e14e4f55e2016
R <merinogeospatial@gmail.com>
2018-06-07 20:56:38 -0400

    logging the returns we need from api

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,16 +1,14 @@
 // Detect - generate face kye
 const dBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/detect?';
-const dApiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
-const dApiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
+const apiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
+const apiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
 let dImageURL = 'image_url=https://pbs.twimg.com/profile_images/650100474226458625/-nngF2Y1_400x400.jpg';
-
 const aBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/face/analyze?';
-const aApiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
-const aApiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
+
 let faceId;
 let aFaceId = '&face_tokens=' + faceId;
-
-let dQueryURL = dBaseURL + dApiKey + dApiSecret + dImageURL;
+let dQueryURL = dBaseURL + apiKey + apiSecret + dImageURL;
+const emotion = "&return_attributes=emotion";
 // let aQueryURL = aBaseURL + aApiKey + aApiSecret + aFaceId;
 
 // }).then(function(aResponse){
@@ -19,7 +17,7 @@ let dQueryURL = dBaseURL + dApiKey + dApiSecret + dImageURL;
 //     // console.log("id", response.request.image_id);
 //     faceId = aResponse.faces[0].face_token;
 
-$.ajax({
+/* $.ajax({
   url: dQueryURL,
   method: "POST",
   success: function () {
@@ -28,8 +26,9 @@ $.ajax({
       method: "POST"
     })
       .then(function (aResponse) {
-        // analysis here
         console.log("Analysis will happen to: ", faceId);
+        var happinessResponse = aResponse.faces[0].attributes.emotion.happiness;
+        console.log(happinessResponse);
       }
       ).then(function (dResponse) {
         console.log(dResponse)
@@ -38,7 +37,36 @@ $.ajax({
       }
       )
   }
-});
+}); */
+
+initialAjax();
+
+function initialAjax() {
+    console.log('initial Ajax fired');
+    $.ajax({
+        url: dQueryURL,
+        method: "POST",
+        success: handleToken
+    });
+}
+
+function handleToken(response) {
+    console.log("Handle Token Fired");
+    faceId = response.faces[0].face_token;
+    aFaceId = '&face_tokens=' + faceId;
+    aQueryURL = aBaseURL + apiKey + apiSecret + aFaceId + emotion;
+    console.log('faceID', faceId);
+    console.log('aQuery', aQueryURL);
+    $.ajax({
+        url: aQueryURL,
+        method: "POST",
+        success: faceData
+    })
+};
+
+function faceData(response) {
+    console.log("response", response);
+}
 
 
-// aResponse.faces[]
+// aResponse.faces[0].attributes.emotion.happiness
