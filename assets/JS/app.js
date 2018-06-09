2bf34b39652432844b26f614260af0d938a49650
Andrew B <awb305@gmail.com>
2018-06-09 00:22:07 -0400

    connected the ajax and handled logic for two players

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,60 +1,79 @@
-////// ID's for HTML tags //////
-// USER INPUT URLS: #user-input1 #user-input-2
-// EMOTION DROPDOWN: #emotion-dropdown
-// USER IMAGES: #image-1 #image-2
-// FINAL SUBMIT BUTTON: #play-button
-// SCORES: wins-1 wins-2 losses-1 lossed-2
+//--------------------------------------------------------------------------
+// Variable and Objects
+//--------------------------------------------------------------------------
 
-$(document).ready(function() {
-    // Detect - generate face key
-    // Replaced the old API key with Jordan's API key, to make sure we aren't sending too many requests
-const apiKey = 'api_key=4AW8dHolxjkb_gEXzZJp8TNc_XG3SBDF';
-const apiSecret = '&api_secret=clKHyanKY2E8wf7CgphWTZgAhTJdUbae';
+
+
+// Detect - generate face kye
 const dBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/detect?';
-let dImageURL = '&image_url=https://pbs.twimg.com/profile_images/650100474226458625/-nngF2Y1_400x400.jpg';
-let dQueryURL = dBaseURL + apiKey + apiSecret + dImageURL;
-
+const dApiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
+const dApiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
+let dImageURL = '';
 
 const aBaseURL = 'https://api-us.faceplusplus.com/facepp/v3/face/analyze?';
+const aApiKey = 'api_key=NrYhlIHltmdXKjxN_XzW2D2UprgtKGnA&';
+const aApiSecret = 'api_secret=QcGMDQPynptvCDh1tmMl5ZTUCYkz6jLv&';
 let faceId;
 let aFaceId = '&face_tokens=' + faceId;
-const emotion = "&return_attributes=emotion";
 
-    // jquery
 
-$("#submit-1").on('click', function() {
-    let playerOneURL = $("#user-input-1").val().trim();
-    $("#image-1").attr('src', playerOneURL);
-    let imgURL1 = 'image_url=' + playerOneURL;
-});
+//the emotion attribute will give us an array of emotion corr values 
+let attribute = 'emotion';
+let aAttribute = '&return_attributes=' + attribute;
 
-$("#submit-2").on('click', function() {
-    let playerTwoURL = $("#user-input-2").val().trim();
-    $("#image-2").attr('src', playerTwoURL);
-    let imgURL2 = 'image_url=' + playerTwoURL;
-});
+//default case is anger
+let emotion = 'anger';
 
-$("#play-button").on('click', function() {
-    let imgOneSrc = $("#image-1").attr('src')
-    let imgTwoSrc = $("#image-2").attr('src')
-    let imgOneSrcAPI = '&image_url' + imgOneSrc;
-    let ImgTwoSrcAPI = '&image_url' + imgTwoSrc;
-    let selectEmotion = $("#emotion-dropdown").find(":selected").text();    
+let dQueryURL = '';
+let aQueryURL = aBaseURL + aApiKey + aApiSecret + aFaceId;
 
-    // This stops the game from playing if the user hasn't uploaded any images and/or hasn't selected an emotion from the dropdown
-    if (imgOneSrc === 'http://via.placeholder.com/200' || imgTwoSrc === 'http://via.placeholder.com/200' || selectEmotion === 'Select an emotion...') {
-        console.log('Please input two images and select an emotion');
-    } else {
-        let dQueryURL = dBaseURL + apiKey + apiSecret + imgOneSrcAPI;
-        initialAjax();
-    }
-});
 
-// ajax
-// initialAjax();
+//handling for different players 
+let ajaxSwitch = false;
 
-function initialAjax() {
+//player objects
+let player1 = {
+    url: '',
+    wins: 0,
+    losses: 0
+};
+let player2 = {
+    url: '',
+    wins: 0,
+    losses: 0
+};
+
+// prevents you from making overlapping ajax calls - see play button onclick event
+var submit = true;
+var go1 = false;
+var go2 = false;
+
+
+//Sample links for testing
+//https://s-i.huffpost.com/gen/2776836/images/o-ANGER-facebook.jpg
+//https://flavorwire.files.wordpress.com/2015/09/o-bill-facebook.jpg?w=1920
+
+
+//--------------------------------------------------------------------------
+// Functions
+//--------------------------------------------------------------------------
+
+function initialAjax(ajaxSwitch) {
     console.log('initial Ajax fired');
+
+    //handles which image URL to call
+    if (!ajaxSwitch) {
+        //uses player 1 url 
+        dImageURL = 'image_url=' + player1.url
+        console.log(dImageURL);
+    } else {
+        //uses player 2 url
+        dImageURL = 'image_url=' + player2.url
+        console.log(dImageURL);
+    }
+
+    dQueryURL = dBaseURL + dApiKey + dApiSecret + dImageURL;
+
     $.ajax({
         url: dQueryURL,
         method: "POST",
@@ -66,7 +85,7 @@ function handleToken(response) {
     console.log("Handle Token Fired");
     faceId = response.faces[0].face_token;
     aFaceId = '&face_tokens=' + faceId;
-    aQueryURL = aBaseURL + apiKey + apiSecret + aFaceId + emotion;
+    aQueryURL = aBaseURL + aApiKey + aApiSecret + aFaceId + aAttribute;
     console.log('faceID', faceId);
     console.log('aQuery', aQueryURL);
     $.ajax({
@@ -76,11 +95,103 @@ function handleToken(response) {
     })
 };
 
+//This is where we handle the emotion object we recieved
 function faceData(response) {
-    console.log("response", response);
+    console.log("resposne", response);
+
+    if (!ajaxSwitch) {
+        // save the emotion score obj to player1     
+        var emotionObj = response.faces[0].attributes.emotion;
+        player1.emotion = emotionObj;
+        //changes to player2
+        if (ajaxSwitch === false) {
+            ajaxSwitch = true;
+            initialAjax(ajaxSwitch);
+        }
+    } else {
+        // save the emotion score obj to player1     
+        var emotionObj = response.faces[0].attributes.emotion;
+        player2.emotion = emotionObj;
+        updateScore();
+    }
 }
 
-// Closing brackets for jQuery document ready below
-});
+function updateScore() {
+    //handling for game result
+    var score1 = player1.emotion[emotion];
+    var score2 = player2.emotion[emotion];
 
-// aResponse.faces[0].attributes.emotion.happiness
+    if (score1 === score2) {
+        console.log('tie');
+    } else if (score1 > score2) {
+        console.log('Player 1 wins');
+        player1.wins++;
+        player2.losses++;
+        console.log("player1 wins", player1);
+        updateHTML();
+
+    } else {
+        console.log('Player 2 wins');
+        player2.wins++;
+        player1.losses++;
+        console.log("player1 wins", player2);
+        updateHTML();
+    }
+}
+
+function updateHTML(player) {
+    var wins1 = $('#wins-1');
+    wins1.text(player1.wins);
+    var wins2 = $('#wins-2');
+    wins2.text(player2.wins);
+    var losses1 = $('#losses-1');
+    losses1.text(player1.losses);
+    var losses2 = $('#losses-2');
+    losses2.text(player2.losses);
+}
+
+function reset() {
+    // allows submit button to be hit again and resets ajax switch 
+    submit = true;
+    ajaxSwitch = false;
+}
+
+//--------------------------------------------------------------------------
+// Main Computation
+//--------------------------------------------------------------------------
+
+//Event handlers to input URL into appropriate player obj & start game 
+$(document).on('click', '#submit-1', function (e) {
+    e.preventDefault;
+    var user1 = $('#user-input-1');
+    var userURL = user1.val();
+    player1.url = userURL;
+    go1 = true;
+
+    //puts images in boxes
+    var image1 = $('#image-1');
+    image1.attr('src', player1.url);
+
+}).on('click', '#submit-2', function (e) {
+    e.preventDefault;
+    var user2 = $('#user-input-2');
+    var userURL = user2.val();
+    player2.url = userURL;
+    go2 = true;
+
+    //puts images in boxes
+    var image1 = $('#image-2');
+    image1.attr('src', player2.url);
+
+}).on('click', '#play-button', function (e) {
+    
+    //sets emotion from dropdown, default one right now is anger
+    emotion = $('#emotion-dropdown :selected').text();
+   
+    // put ajaxSwitch error handling here
+    if (go1 && go2 && submit) {
+        initialAjax(ajaxSwitch);
+    }else{
+        alert('Be-Boop it looks like you did not submit two URLs');
+    }
+})
